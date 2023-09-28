16b863c553ce75061337541ab68e36b2265e9b1a
Vinayak A <ajithvinu23@gmail.com>
2023-09-28 07:44:31 +0530

    api fix

--- frontend/src/Components/ConfirmReorder/ConfirmReorder.jsx
+++ frontend/src/Components/ConfirmReorder/ConfirmReorder.jsx
@@ -3,10 +3,14 @@ import {  useSelector,useDispatch } from "react-redux";
 import { setImgData } from "../../Redux/imgSlice";
 import { setDragDropIndex } from "../../Redux/DragDropSlice";
 import instance from "../../services/axios";
+import { useState } from "react";
+import SpinnerMini from "../SpinnerMini/SpinnerMini";
+import getDataApi from "../../services/getImageApi";
 const ConfirmReOrder=()=>{
 const toggle=useSelector(store=>store.DragDrop);
 const userData=useSelector(store=>store.user)
-const reArrangedImage=useSelector(store=>store.Image)
+const reArrangedImage=useSelector(store=>store.Image);
+const [isLoading,setIsLoading]=useState(false)
 const dispatch=useDispatch()
 
 
@@ -25,8 +29,13 @@ const dispatch=useDispatch()
         try {
             console.log(reArrangedImage)
             
-            const data=await instance.put(`/img/reorder/${userData.uid}`,reArrangedImage);
-            console.log(data)
+            const {data,status}=await instance.put(`/img/reorder/${userData.uid}`,reArrangedImage);
+            console.log(data);
+            console.log(status);
+            if(status===200){
+                getDataApi(dispatch);
+                dispatch(setDragDropIndex(null));
+            }
         } catch (error) {
             console.log(error)
         } finally{
@@ -39,8 +48,8 @@ const dispatch=useDispatch()
     return (
         <div className="fixed top-[87%] bg-[#dff7e5] left-0 w-[100%] bottom-0 flex flex-col justify-center items-end">
             <div className="flex gap-10 mr-7 ">
-                <button className="px-4 py-2  bg-[#92e3a9] text-center cursor-pointer active:translate-y-1 transition-all rounded-sm" onClick={handleSubmit}>Submit</button>
-                <button className="px-4 py-2  bg-[#92e3a9] text-center cursor-pointer active:translate-y-1 transition-all rounded-sm" onClick={handleClickCancel}>Cancel</button>
+                <button className="px-4 py-2  bg-[#92e3a9] text-center cursor-pointer active:translate-y-1 transition-all rounded-sm" onClick={handleSubmit} disabled={isLoading}>{isLoading ? <SpinnerMini/> : "Submit"}</button>
+                <button className="px-4 py-2  bg-[#92e3a9] text-center cursor-pointer active:translate-y-1 transition-all rounded-sm" onClick={handleClickCancel} disabled={isLoading}>Cancel</button>
             </div>
         </div>
     );
